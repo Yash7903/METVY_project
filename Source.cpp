df08bfc33816597568f505509a22933022322edf
Swarup Mohapatra <1930159@kiit.ac.in>
2021-01-06 02:07:16 +0530

    METVY_PROJECT

--- Source.cpp
+++ Source.cpp
@@ -1,8 +1,12 @@
 
 #include<iostream>
 #include<stdlib.h>
+#include<limits>
 #include<cmath>
+#include<ctype.h>
+#define PI 3.141
 using namespace std;
+
 void sum();
 void product();
 void sub();
@@ -24,6 +28,8 @@ void log();
 void log10();
 void atan2();
 void pow();
+void ftoC();
+void volume();
 int main()
 
 {
@@ -32,219 +38,258 @@ int main()
 	char control='y';
 	//provides user the choice between switching to normal calculator or the scientific version//
 	while (control == 'y' || control == 'Y') {
-		cout << "\t\t..MENU..\n\t1.<STANDARD CALCULATOR>\n\t2.<SCIENTIFIC CALCULATOR>" << endl;
-		cin >> loop_ctrl;
+		cout<< "\t\t..MENU..\n\t1.<STANDARD CALCULATOR>\n\t2.<SCIENTIFIC CALCULATOR>" << endl;
+		cin>> loop_ctrl;
 		if (loop_ctrl != 1 && loop_ctrl != 2)
 		{
-			cout << "error wrong option entered \n" << "try again ?(y/n)";
-			cin >> control;
+			cout<< "error wrong option entered \n" << "try again ?(y/n)";
+			cin>> control;
 
 		}
 		else
 		{
 			while (loop_ctrl == 1)
 			{
-				cout << "enter desired operation using key provided in brackets '()' :\n";
+				cout<< "enter desired operation using key provided in brackets '()' :\n";
 
-				cout << "(1) addition \n";
+				cout<< "(1) addition \n";
 
-				cout << "(2) multiplication\n";
+				cout<< "(2) multiplication\n";
 
-				cout << "(3) subtraction \n";
+				cout<< "(3) subtraction \n";
 
-				cout << "(4) division\n";
-				
-				cin >> function_ctrl;
-				
+				cout<< "(4) division\n";
+			
+				//avoid infinite loop when a character is entered and corrupts the int cin//
+				if (!(cin>> function_ctrl)) 
+				{
+					cin.clear();
+					cin.ignore(numeric_limits<streamsize>::max() ); 
+			
+				}
+
+				if (function_ctrl > 4 || function_ctrl< 1) 
+				{
+					function_ctrl = 0;
+					cout<< "\n you have entered a wrong choice\n"; break;
+					
+				} 
+				cin>> function_ctrl;
 				break;
 
 
 			}
 			while (loop_ctrl == 2)
 			{
-				cout << "enter desired operation using key provided in brackets '()'\nscientific options key is (s.number) :\n";
+				cout<< "enter desired operation using key provided in brackets '()'\nscientific options key is (s.number) :\n";
 
-				cout << "(1) addition \n";
+				cout<< "(1) addition \n";
 
-				cout << "(2) multiplication\n";
+				cout<< "(2) multiplication\n";
 
-				cout << "(3) subtraction \n";
+				cout<< "(3) subtraction \n";
 
-				cout << "(4) division\n\n\n";
+				cout<< "(4) division\n<<SCEINTIFIC OPERATIONS>>\n";
 
-				cout << "(5) remainder\n";
+				cout<< "(5) remainder\n";
 
-				cout << "(6) square\n";
+				cout<< "(6) square\n";
 
-				cout << "(7)  cube\n";
+				cout<< "(7)  cube\n";
 
-				cout << "(8)  cosine\n";
+				cout<< "(8)  cosine\n";
 
-				cout << "(9)  sine\n";
+				cout<< "(9)  sine\n";
 
-				cout << "(10)  tangent\n";
+				cout<< "(10)  tangent\n";
 
-				cout << "(11) hyperbolic tangent\n";
+				cout<< "(11) hyperbolic tangent\n";
 
-				cout << "(12)  hyperbolic cosine\n";
+				cout<< "(12)  hyperbolic cosine\n";
 
-				cout << "(13)  hyperbolic sine\n";
+				cout<< "(13)  hyperbolic sine\n";
 
-				cout << "(14)  arc cosine\n";
+				cout<< "(14)  arc cosine\n";
 
-				cout << "(15)  arc sine\n";
+				cout<< "(15)  arc sine\n";
 
-				cout << "(16)  arc tangential\n";
+				cout<< "(16)  arc tangential\n";
 
-				cout << "(17) exponent\n";
+				cout<< "(17) exponent\n";
 
-				cout << "(18)  logarithm\n";
+				cout<< "(18)  logarithm\n";
 
-				cout << "(19)  common logarithm i.e log10\n";
+				cout<< "(19)  common logarithm i.e log10\n";
 
-				cout << "(20)  atan2\n";
+				cout<< "(20)  atan2\n";
 
-				cout << "(21)  power\n";
+				cout<< "(21)  power\n<<MEASUREMENT OPERATIONS>>\n";
 
-				cin >> function_ctrl;
+				cout<<"farenheit to celsius conversion";
 
+				
+				//avoid infinite loop when a character is entered and corrupts the int cin//
+				if (!(cin>> function_ctrl))
+				{
+					cin.clear();
+					cin.ignore(numeric_limits<streamsize>::max(), '\n');
+					cout<< "you have entered a wrong choice";
+				}
+				cout<< "\ncheck complete please valid choice";
 				break;
-
-
 
 			}
 		}
 
-		if (function_ctrl == 1)
+		switch (function_ctrl)
+
+		{
+		case 1:
 		{
 			sum();
-			cout << "-----exiting------\n";
+			cout<< "-----exiting------\n";
 		}
 
-		else if (function_ctrl == 2)
+		case 2:
 		{
 			product();
-			cout << "-----exiting------\n";
+			cout<< "-----exiting------\n";
 		}
 
-		else if (function_ctrl == 3)
+		case 3:
 		{
 			sub();
-			cout << "-----exiting------\n";
+			cout<< "-----exiting------\n";
 		}
 
-		if (function_ctrl == 4)
+		case 4:
 
 		{
 			div();
-			cout << "-----exiting------\n";
+			cout<< "-----exiting------\n";
 		}
 
-		else if (function_ctrl == 5)
+		case 5:
 
 		{
 			remainder();
-			cout << "-----exiting------\n";
+			cout<< "-----exiting------\n";
 		}
 
-		else if (function_ctrl == 6)
+		case 6:
 		{
 			square();
-			cout << "-----exiting------\n";
+			cout<< "-----exiting------\n";
 		}
-		else if (function_ctrl == 7)
+		case 7:
 		{
 			cube();
-			cout << "-----exiting------\n";
+			cout<< "-----exiting------\n";
 		}
 
-		else if (function_ctrl == 8)
+		case 8:
 		{
 			cos();
-			cout << "-----exiting------\n";
+			cout<< "-----exiting------\n";
 		}
 
-		else if (function_ctrl == 9)
+		case 9:
 		{
 			sin();
-			cout << "-----exiting------\n";
+			cout<< "-----exiting------\n";
 		}
 
-		else if (function_ctrl == 10)
+		case 10:
 		{
 			tan();
-			cout << "-----exiting------\n";
+			cout<< "-----exiting------\n";
 		}
 
-		else if (function_ctrl == 11)
+		case 11:
 		{
 			cosh();
-			cout << "-----exiting------*\n";
+			cout<< "-----exiting------*\n";
 		}
 
-		else if (function_ctrl == 12)
+		case 12:
 
 		{
 			sinh();
-			cout << "-----exiting------\n";
+			cout<< "-----exiting------\n";
 		}
 
-		else if (function_ctrl == 13)
+		case 13:
 		{
 			tanh();
-			cout << "-----exiting------\n";
+			cout<< "-----exiting------\n";
 		}
 
-		else if (function_ctrl == 14)
+		case 14:
 		{
 			acos();
-			cout << "-----exiting------\n";
+			cout<< "-----exiting------\n";
 		}
 
-		else if (function_ctrl == 15)
+		case 15:
 		{
 			asin();
-			cout << "-----exiting------\n";
+			cout<< "-----exiting------\n";
 		}
 
-		else if (function_ctrl == 16)
+		case 16:
 		{
 			atan();
-			cout << "-----exiting------\n";
+			cout<< "-----exiting------\n";
 		}
 
-		else if (function_ctrl == 17)
+		case 17:
 		{
 			exp();
-			cout << "-----exiting------*\n";
+			cout<< "-----exiting------*\n";
 		}
-		else if (function_ctrl == 18)
+		case 18:
 		{
 			log();
-			cout << "-----exiting------\n";
+			cout<< "-----exiting------\n";
 		}
 
-		else if (function_ctrl == 19)
+		case 19:
 		{
 			log10();
-			cout << "-----exiting------\n";
+			cout<< "-----exiting------\n";
 		}
 
-		else if (function_ctrl == 20)
+		case 20:
 
 		{
 			atan2();
-			cout << "-----exiting------\n";
+			cout<< "-----exiting------\n";
 		}
 
-		else if (function_ctrl == 21)
+		case 21:
 		{
 			pow();
-			cout << "-----exiting------\n";
+			cout<< "-----exiting------\n";
 		}
-
-		cout << "want to continue? (y/n)";
-		cin >> control;
+		case 22:
+		{
+			ftoC();
+			cout<< "-----exiting------\n";
+		}
+		case 23:
+		{
+			volume();
+			cout<< "-----exiting------\n";
+		}
+	
+		default :
+		{
+			cout<<"wrong choice entered";
+		}
+	
+		}
+		cout<< "want to continue? (y/n)";
+		cin>> control;
 	}
 }
 
@@ -252,224 +297,269 @@ void sum()
 
 {
 	int a, b, sum;
-	cout << "\nenter the first number\n";
-	cin >> a;
-	cout << "\nenter the second number\n";
-	cin >> b;
+	cout<< "\nenter the first number\n";
+	cin>> a;
+	cout<< "\nenter the second number\n";
+	cin>> b;
 	sum = a + b;
-	cout << "sum is" << sum << endl;
+	cout<< "sum is" << sum << endl;
 }
 
 void product()
 
 {
 	int a, b, product;
-	cout << "\nenter the first number\n";
-	cin >> a;
-	cout << "\nenter the second number\n";
-	cin >> b;
+	cout<< "\nenter the first number\n";
+	cin>> a;
+	cout<< "\nenter the second number\n";
+	cin>> b;
 	product = a * b;
-	cout << "product is" << product << endl;
+	cout<< "product is" << product << endl;
 }
 
 void sub()
 
 {
 	int a, b, sub;
-	cout << "\nenter the first number\n";
-	cin >> a;
-	cout << "\nenter the second number\n";
-	cin >> b;
+	cout<< "\nenter the first number\n";
+	cin>> a;
+	cout<< "\nenter the second number\n";
+	cin>> b;
 	sub = a - b;
-	cout << "subtraction is" << sub << endl;
+	cout<< "subtraction is" << sub << endl;
 }
 
 void div()
 
 {
-	int a, b, div;
-	cout << "\nenter the first number\n";
-	cin >> a;
-	cout << "\nenter the second number\n";
-	cin >> b;
+	double a, b, div;
+	cout<< "\nenter the first number\n";
+	cin>> a;
+	cout<< "\nenter the second number\n";
+	cin>> b;
 	div = a / b;
-	cout << "division is" << div << endl;
+	cout<< "division is" << div << endl;
 }
 
 void remainder()
 
 {
 	int a, b, remainder;
-	cout << "\nenter the first number\n";
-	cin >> a;
-	cout << "\nenter the second number\n";
-	cin >> b;
+	cout<< "\nenter the first number\n";
+	cin>> a;
+	cout<< "\nenter the second number\n";
+	cin>> b;
 	remainder = a * b;
-	cout << "remainder is" << remainder << endl;
+	cout<< "remainder is" << remainder << endl;
 }
 
 void square()
 
 {
 	int a, square;
-	cout << "\nenter the number\n";
-	cin >> a;
+	cout<< "\nenter the number\n";
+	cin>> a;
 	square = a * a;
-	cout << "square is" << square << endl;
+	cout<< "square is" << square << endl;
 }
 
 void cube()
 
 {
 	int a, cube;	
-	cout << "\nenter the number\n";
-	cin >> a;
+	cout<< "\nenter the number\n";
+	cin>> a;
 	cube = a * a * a;
-	cout << "cube is" << cube << endl;
+	cout<< "cube is" << cube << endl;
 }
 
 void cos()
 
 {
-	double a, result, PI = 3.14159265;
-	cout << "\nenter the angle\n";
-	cin >> a;
+	double a, result ;
+	cout<< "\nenter the angle\n";
+	cin>> a;
 	result = cos(a * PI / 180);
-	cout << "the cosine of " << a << " is " << result << endl;
+	cout<< "the cosine of " << a << " is " << result << endl;
 }
 
 void sin()
 
 {
-	double a, result, PI = 3.14159265;
-	cout << "\nenter the angle\n";
-	cin >> a;
+	double a, result ;
+	cout<< "\nenter the angle\n";
+	cin>> a;
 	result = sin(a * PI / 180);
-	cout << "the sine of " << a << " is " << result << endl;
+	cout<< "the sine of " << a << " is " << result << endl;
 }
 
 void tan()
 
 {
-	double a, result, PI = 3.14159265;
-	cout << "\nenter the angle\n";
-	cin >> a;
+	double a, result ;
+	cout<< "\nenter the angle\n";
+	cin>> a;
 	result = tan(a * PI / 180);
-	cout << "the tan of the " << a << " is " << result << endl;
+	cout<< "the tan of the " << a << " is " << result << endl;
 }
 
 void cosh()
 
 {
-	double a, result, PI = 3.14159265;
-	cout << "\nenter the angle\n";
-	cin >> a;
+	double a, result ;
+	cout<< "\nenter the angle\n";
+	cin>> a;
 	result = cosh(a);
-	cout << "the hyperbolic cosine of " << a << " is " << result << endl;
+	cout<< "the hyperbolic cosine of " << a << " is " << result << endl;
 }
 
 void sinh()
 
 {
-	double a, result, PI = 3.14159265;
-	cout << "\nenter the angle\n";
-	cin >> a;
+	double a, result ;
+	cout<< "\nenter the angle\n";
+	cin>> a;
 	result = sinh(a);
-	cout << "the hyperbolic sine of " << a << " is " << result << endl;
+	cout<< "the hyperbolic sine of " << a << " is " << result << endl;
 }
 
 void tanh()
 
 {
 	
-	double a, result, PI = 3.14159265;
-	cout << "\nenter the angle\n";
-	cin >> a;
+	double a, result ;
+	cout<< "\nenter the angle\n";
+	cin>> a;
 	result = tanh(a);
-	cout << "The hyperbolic tangent of " << a << " is " << result << endl;
+	cout<< "The hyperbolic tangent of " << a << " is " << result << endl;
 }
 
 void acos()
 
 {
-	double a, result, PI = 3.14159265;
-	cout << "\nenter the angle\n";
-	cin >> a;
+	double a, result ;
+	cout<< "\nenter the angle\n";
+	cin>> a;
 	result = acos(a) * 180.0 / PI;
-	cout << "The arc cosine of " << a << " is " << result << endl;
+	cout<< "The arc cosine of " << a << " is " << result << endl;
 }
 
 void asin()
 
 {
-	double a, result, PI = 3.14159265;
-	cout << "\nenter the angle\n";
-	cin >> a;
+	double a, result ;
+	cout<< "\nenter the angle\n";
+	cin>> a;
 	result = asin(a) * 180.0 / PI;
-	cout << "The arc sine of " << a << " is " << result << endl;
+	cout<< "The arc sine of " << a << " is " << result << endl;
 }
 
 void atan()
 
 {
-	double a, result, PI = 3.14159265;
-	cout << "\nenter the angle\n";
-	cin >> a;
+	double a, result ;
+	cout<< "\nenter the angle\n";
+	cin>> a;
 	result = tanh(a);
-	cout << "The arc tangential of " << a << " is " << result << endl;
+	cout<< "The arc tangential of " << a << " is " << result << endl;
 }
 
 void exp()
 
 {
 	double a, result;
-	cout << "\nenter the number\n";
-	cin >> a;
+	cout<< "\nenter the number\n";
+	cin>> a;
 	result = exp(a);
-	cout << "The exponential value of " << a << " is = " << result << endl;
+	cout<< "The exponential value of " << a << " is = " << result << endl;
 }
 
 void log()
 
 {
 	double a, result;
-	cout << "\nenter the value to take a log \n";
-	cin >> a;
+	cout<< "\nenter the value to take a log \n";
+	cin>> a;
 	result = log(a);
-	cout << "The logarithm of " << a << " is " << result << endl;
+	cout<< "The logarithm of " << a << " is " << result << endl;
 }
 
 void log10()
 
 {
 	double a, result;
-	cout << "\nenter the value to take a log \n";
-	cin >> a;
+	cout<< "\nenter the value to take a log \n";
+	cin>> a;
 	result = log10(a);
-	cout << "The common logarithm of " << a << " is " << result << endl;
+	cout<< "The common logarithm of " << a << " is " << result << endl;
 }
 
 void atan2()
 
 {
-	float a, b, result, PI = 3.14;
-	cout << "\nenter the first number\n";
-	cin >> a;
-	cout << "\nenter second the number\n";
-	cin >> b;
+	double a, b, result ;
+	cout<< "\nenter the first number\n";
+	cin>> a;
+	cout<< "\nenter second the number\n";
+	cin>> b;
 	result = atan2(a, b) * 180 / PI;
-	cout << "The arc tangent for " << a << " and " << b << " is " << result << endl;
+	cout<< "The arc tangent for " << a << " and " << b << " is " << result << endl;
 	
 }
 
 void pow()
 {
 	int a, b, power;
-	cout << "\nenter the number\n";
-	cin >> a;
-	cout << "\nenter the power\n";
-	cin >> b;
+	cout<< "\nenter the number\n";
+	cin>> a;
+	cout<< "\nenter the power\n";
+	cin>> b;
 	power = pow(a, b);
-	cout << "\n" << a << " raise to power " << b << " is " << power;
+	cout<< "\n" << a << " raise to power " << b << " is " << power;
 
+}
+void fToC()
+ {
+	 double fahrenheit,celsius;
+	cout<<"ëneter the temperature in farenheit";
+     celsius = (fahrenheit - 32.0) * 5.0 / 9.0;
+     cout<<"the temperature in celsius is "<<celsius;
+ }
+void volume ()
+{
+	double x1,x2,x3;
+	int ch;
+	cout<<"\n\t\t1>cube\n\t\t2>cuboid\n\t\t3>sphere\n\t\tcylinder";
+	cin>>ch;
+	switch (ch)
+	{
+		case 1:
+		{
+			cout<<"\nenter the side length";
+			cin>>x1;
+			cout<<"\nthe volume is "<<x1*x1*x1;
+		}
+		case 2:
+		{
+			cout<<"\nenter the length , heigth and breadth of cuboid ";
+			cin>>x1>>x2>>x3;
+			cout<<"\nthe volume is "<<x1*x2*x3;
+		}
+		case 3:
+		{
+			cout<<"\nenter the radius";
+			cin>>x1;
+			cout<<"\nthe volume is "<<x1*(4/3)*x1*x1*PI;
+		}
+		case 4:
+		{
+			cout<<"enter the radius and heigth";
+			cin>>x1>>x2;
+			cout<<"\nthe volume is "<<x1*x2*x1*PI;
+		}
+		default:
+		{
+			cout<<"wrong choice entered";
+		}
+	}
 }
